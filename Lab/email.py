101e02de6ffb733904554e88f7b9443ed1f3f871
Georgi Angelov <gtangelov76@gmail.com>
2026-02-19 23:34:48 +0200

    Update email.py

---
+++ Lab/email.py
@@ -0,0 +1,13 @@
+class Email:
+    def __init__(self, sender, receiver, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+        is_sent = False
+
+    def send(self):
+        pass
+
+    def get_info(self):
+        pass
+    
